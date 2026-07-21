52f4d29cd9826eb8cea3c92f3e07634a811ff8ad
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-07-21 17:30:07 +0200

    feat: Implemented mock Auth Service

--- Frontend/atlas/src/app/features/auth/services/auth-service.ts
+++ Frontend/atlas/src/app/features/auth/services/auth-service.ts
@@ -1,4 +1,44 @@
-import { Service } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { AuthResponse, LoginRequest, RegisterRequest } from '../models/auth-response.model';
+import { Observable, of, tap } from 'rxjs';
 
-@Service()
-export class AuthService {}
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    private loggedIn = signal(false);
+
+    constructor() { }
+
+    login(request: LoginRequest): Observable<AuthResponse> {
+        const mockResponse: AuthResponse = {
+            token: 'mock-token',
+            refreshToken: 'mock-refresh',
+            expiresAt: new Date()
+        };
+
+        return of(mockResponse).pipe(
+            tap(() => this.loggedIn.set(true))
+        );
+    }
+
+    register(req: RegisterRequest): Observable<AuthResponse> {
+        const mockResponse: AuthResponse = {
+            token: 'mock-token',
+            refreshToken: 'mock-refresh',
+            expiresAt: new Date()
+        };
+
+        return of(mockResponse).pipe(
+            tap(() => this.loggedIn.set(true))
+        );
+    }
+
+    logout(): void {
+        this.loggedIn.set(false);
+    }
+
+    isAuthenticated(): boolean {
+        return this.loggedIn();
+    }
+}

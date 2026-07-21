3bcef3d676889da2c80f822339ebd247bffb45d5
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-07-21 19:04:57 +0200

    feat: Added registration component

--- Frontend/atlas/src/app/features/auth/components/register/register-component.ts
+++ Frontend/atlas/src/app/features/auth/components/register/register-component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideGalleryVerticalEnd } from '@ng-icons/lucide';
+import { RegisterForm } from './register-form';
 
 @Component({
   selector: 'spartan-register-component',
-  imports: [],
+  imports: [RegisterForm, NgIcon, RouterLink],
+  providers: [provideIcons({ lucideGalleryVerticalEnd })],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'block',
+  },
   templateUrl: './register-component.html',
   styleUrl: './register-component.css',
 })
-export class RegisterComponent {}
+export class RegisterComponent { }

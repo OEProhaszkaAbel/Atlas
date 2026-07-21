8a6053c00a9e4d9580fcae8a7f32071f57064bd0
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-07-21 19:04:37 +0200

    feat: Added login component

--- Frontend/atlas/src/app/features/auth/components/login/login-component.ts
+++ Frontend/atlas/src/app/features/auth/components/login/login-component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideGalleryVerticalEnd } from '@ng-icons/lucide';
+import { LoginForm } from './login-form';
 
 @Component({
   selector: 'spartan-login-component',
-  imports: [],
+  imports: [RouterLink, LoginForm, NgIcon],
+  providers: [provideIcons({ lucideGalleryVerticalEnd })],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'block',
+  },
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
-export class LoginComponent {}
+export class LoginComponent { }

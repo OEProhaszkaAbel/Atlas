0d5d06073a093524200b8fb302679093ae3c7013
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-07-21 17:28:47 +0200

    feat: Implemented Auth Guard

--- Frontend/atlas/src/app/features/auth/guards/auth-guard.ts
+++ Frontend/atlas/src/app/features/auth/guards/auth-guard.ts
@@ -1,5 +1,9 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  return auth.isAuthenticated() ? true : router.navigate(['/auth/login']);
 };

2a0d4fe7b113593f435255566a1ca36f20d2cc55
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-07-29 17:18:48 +0200

    Added workout related components

---
+++ Frontend/atlas/src/app/features/gym/components/workout-history/workout-history-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spartan-workout-history-component',
+  imports: [],
+  templateUrl: './workout-history-component.html',
+  styleUrl: './workout-history-component.css',
+})
+export class WorkoutHistoryComponent {}
---
+++ Frontend/atlas/src/app/features/gym/components/workout/workout-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spartan-workout-component',
+  imports: [],
+  templateUrl: './workout-component.html',
+  styleUrl: './workout-component.css',
+})
+export class WorkoutComponent {}

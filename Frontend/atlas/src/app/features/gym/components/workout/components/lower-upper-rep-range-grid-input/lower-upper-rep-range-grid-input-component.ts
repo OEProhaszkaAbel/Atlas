883b0d4d65cdc403be0daf39d3f679ad7cdf0eb4
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-07-29 21:45:08 +0200

    Init lower-upper-rep-range-grid-input

---
+++ Frontend/atlas/src/app/features/gym/components/workout/components/lower-upper-rep-range-grid-input/lower-upper-rep-range-grid-input-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spartan-lower-upper-rep-range-grid-input-component',
+  imports: [],
+  templateUrl: './lower-upper-rep-range-grid-input-component.html',
+  styleUrl: './lower-upper-rep-range-grid-input-component.css',
+})
+export class LowerUpperRepRangeGridInputComponent {}

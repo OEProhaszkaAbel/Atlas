cd78d3568aa9c1d1be262b70b0e1ad3544d95fb4
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-07-29 20:28:23 +0200

    Init exercise-picker-component

---
+++ Frontend/atlas/src/app/features/gym/components/workout/components/exercise-picker/exercise-picker-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spartan-exercise-picker-component',
+  imports: [],
+  templateUrl: './exercise-picker-component.html',
+  styleUrl: './exercise-picker-component.css',
+})
+export class ExercisePickerComponent {}

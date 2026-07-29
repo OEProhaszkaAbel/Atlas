3b74cf3233d0e469e76fca9015997d52623ecbdf
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-07-29 21:00:40 +0200

    Init RepRangeInput

---
+++ Frontend/atlas/src/app/features/gym/components/workout/components/rep-range-input/rep-range-input-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spartan-rep-range-input-component',
+  imports: [],
+  templateUrl: './rep-range-input-component.html',
+  styleUrl: './rep-range-input-component.css',
+})
+export class RepRangeInputComponent {}

53fb8cac298449926a176b237696380c8a5c8581
Prohaszka Abel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-08-05 21:37:14 +0200

    Added LowerUpperRepRangeGridInputComponent

--- Frontend/atlas/src/app/features/gym/components/workout/components/lower-upper-rep-range-grid-input/lower-upper-rep-range-grid-input-component.ts
+++ Frontend/atlas/src/app/features/gym/components/workout/components/lower-upper-rep-range-grid-input/lower-upper-rep-range-grid-input-component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HlmFieldImports } from '@spartan-ng/helm/field';
+import { HlmInputImports } from '@spartan-ng/helm/input';
+import { RepRangeInputComponent } from '../rep-range-input/rep-range-input-component';
 
 @Component({
   selector: 'spartan-lower-upper-rep-range-grid-input-component',
-  imports: [],
+  imports: [HlmInputImports, HlmFieldImports, RepRangeInputComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'min-w-xs sm:min-w-sm' },
   templateUrl: './lower-upper-rep-range-grid-input-component.html',
   styleUrl: './lower-upper-rep-range-grid-input-component.css',
 })
-export class LowerUpperRepRangeGridInputComponent {}
+export class LowerUpperRepRangeGridInputComponent { }

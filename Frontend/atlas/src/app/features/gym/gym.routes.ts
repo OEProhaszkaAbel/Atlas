22e9e6e5cacaaaa6f66fb86bc6b2301c5f567278
prohaszkaAbel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-09-05 00:12:01 +0200

    Refreshed routes

--- Frontend/atlas/src/app/features/gym/gym.routes.ts
+++ Frontend/atlas/src/app/features/gym/gym.routes.ts
@@ -4,19 +4,19 @@ export const GYM_ROUTES: Routes = [
     {
         path: 'workout',
         loadComponent: () =>
-            import('./components/workout/workout-component')
+            import('./pages/workout/workout-component')
                 .then(c => c.WorkoutComponent)
     },
     {
         path: 'progress',
         loadComponent: () =>
-            import('./components/progress/progress-component')
+            import('./pages/progress/progress-component')
                 .then(c => c.ProgressComponent)
     },
     {
         path: 'workout-history',
         loadComponent: () =>
-            import('./components/workout-history/workout-history-component')
+            import('./pages/workout-history/workout-history-component')
                 .then(c => c.WorkoutHistoryComponent)
     }
 ];

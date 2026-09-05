795603fb22668d1b6a51877fad69eabd2808e3a1
prohaszkaAbel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-09-05 21:38:07 +0200

    Added id

--- Frontend/atlas/src/app/features/gym/models/Exercises/SetType.ts
+++ Frontend/atlas/src/app/features/gym/models/Exercises/SetType.ts
@@ -1,22 +1,27 @@
 export interface SetType {
+  id: number;
   name: string;
   description: string;
 }
 
 export const SET_TYPES: SetType[] = [
   {
+    id: 1,
     name: 'Normal Set',
     description: 'A standard set of exercises performed in a workout.',
   },
   {
+    id: 2,
     name: 'Drop Set',
     description: 'A set where the weight is reduced after reaching failure.',
   },
   {
+    id: 3,
     name: 'Super Set',
     description: 'Two exercises performed in succession targeting opposing muscle groups.',
   },
   {
+    id: 4,
     name: 'Warmup Set',
     description: 'A set performed at a lower intensity to prepare the muscles for heavier lifting.',
   },

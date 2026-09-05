9574fdaf3b9822fcc2f4b4b5598dc1b54d55c030
prohaszkaAbel <prohaszka.abel.ferenc@stud.uni-obuda.hu>
2026-09-05 11:29:22 +0200

    Added working prototype for workout-exercise

--- Frontend/atlas/src/app/features/gym/components/workout-exercise/workout-exercise.ts
+++ Frontend/atlas/src/app/features/gym/components/workout-exercise/workout-exercise.ts
@@ -1,9 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { ExerciseSet } from '@gym-models/Exercises/ExerciseSet';
+import { RepRange } from '@gym-models/Exercises/RepRange';
+import { RestTime } from '@gym-models/Exercises/RestTime';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideChevronDown, lucideChevronUp, lucidePlus } from '@ng-icons/lucide';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmCardImports } from '@spartan-ng/helm/card';
+import { HlmSeparator } from '@spartan-ng/helm/separator';
+import { ExerciseSetComponent } from '../exercise-set-component/exercise-set-component';
 
 @Component({
   selector: 'workout-exercise',
-  imports: [],
+  imports: [HlmCardImports, HlmButtonImports, NgIcon, HlmSeparator, ExerciseSetComponent],
+  providers: [provideIcons({ lucidePlus, lucideChevronDown, lucideChevronUp })],
   templateUrl: './workout-exercise.html',
   styleUrl: './workout-exercise.css',
 })
-export class WorkoutExercise {}
+export class WorkoutExercise {
+  public isExpanded = signal(false);
+  public isCompleted = signal(false);
+  public isEditing = signal(false);
+  public sets = signal<ExerciseSet[]>([
+    {
+      id: 1,
+      isCompleted: false,
+      set_type_id: 3,
+      weight: 0,
+      reps: 0,
+      repRange: {} as RepRange,
+      restTime: {} as RestTime,
+    } as ExerciseSet,
+  ]);
+  public isSetsEmpty = computed(() => this.sets().length === 0);
+
+  toggleExpanded() {
+    this.isExpanded.set(!this.isExpanded());
+  }
+
+  completedSet(set: ExerciseSet) {
+    console.log('Completed set:', set);
+  }
+
+  deleteSet(set: ExerciseSet) {
+    this.sets.update((sets) => sets.filter((s) => s !== set));
+    console.log(this.sets.length);
+  }
+
+  addSet() {
+    this.sets.update((sets) => [
+      ...sets,
+      {
+        id: 0,
+        isCompleted: false,
+        set_type_id: 0,
+        weight: 0,
+        reps: 0,
+        repRange: {} as RepRange,
+        restTime: {} as RestTime,
+      } as ExerciseSet,
+    ]);
+  }
+}
